2c264acc75d47fa9cb58f2d5129e733667dda5c6
Florian Wolters <wolters.fl@gmail.com>
2013-03-01 14:32:08 +0100

    Updated API documentation to latest version.

--- elementlist.js
+++ elementlist.js
@@ -1,3 +1,3 @@
 
 var ApiGen = ApiGen || {};
-ApiGen.elements = [["c","FlorianWolters\\Application\\Chat\\Command\\RunServerCommand"],["c","FlorianWolters\\Application\\Chat\\Model\\Message"],["c","FlorianWolters\\Application\\Chat\\Server"],["c","FlorianWolters\\Application\\Chat\\ServerApplication"]];
+ApiGen.elements = [["c","DateTime"],["c","Exception"],["c","FlorianWolters\\Application\\Chat\\Command\\RunServerCommand"],["c","FlorianWolters\\Application\\Chat\\Model\\Message"],["c","FlorianWolters\\Application\\Chat\\Server"],["c","FlorianWolters\\Application\\Chat\\ServerApplication"]];
